574b4c7ca2dc716125fb701c76606d01e9e9992a
Arnold-Mollo <amollo@techeraperu.com>
2023-07-03 12:45:39 -0500

    Add eureka-server

---
+++ registry-service/src/test/java/learning/digitallab/store/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.digitallab.store.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
