5fac5b17cef31553de7f27b4669b265b20b18240
Arnold Mollo <amolloa.147@gmail.com>
2023-11-01 10:49:57 -0500

    Fixes: data de H2

--- customer-service/src/main/resources/data.sql
+++ customer-service/src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO tbl_regions (id, name) VALUES (1, 'South America');
-INSERT INTO tbl_regions (id, name) VALUES (2, 'Central America');
-INSERT INTO tbl_regions (id, name) VALUES (3, 'North America');
-INSERT INTO tbl_regions (id, name) VALUES (4, 'Europe');
+INSERT INTO tbl_regions (id, name) VALUES (1, 'Sudamerica');
+INSERT INTO tbl_regions (id, name) VALUES (2, 'Centroamerica');
+INSERT INTO tbl_regions (id, name) VALUES (3, 'Norteamerica');
+INSERT INTO tbl_regions (id, name) VALUES (4, 'Europa');
 INSERT INTO tbl_regions (id, name) VALUES (5, 'Asia');
 INSERT INTO tbl_regions (id, name) VALUES (6, 'Africa');
 INSERT INTO tbl_regions (id, name) VALUES (7, 'Oceania');
-INSERT INTO tbl_regions (id, name) VALUES (8, 'Antarctica');
+INSERT INTO tbl_regions (id, name) VALUES (8, 'Antartida');
 
-INSERT INTO tbl_customers (number_id,first_name,last_name , email, photo_url, region_id, status) VALUES('32404580', 'Andres', 'Guzman', 'profesor@bolsadeideas.com','',1,'CREATED');
+--INSERT INTO tbl_customers (number_id,first_name,last_name , email, photo_url, region_id, status) VALUES('32404580', 'Andres', 'Guzman', 'profesor@bolsadeideas.com','',1,'CREATED');
